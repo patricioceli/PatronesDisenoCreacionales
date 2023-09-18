e2ad1b174cd909aed8124c2b1f1719d7b579e0f6
patricioceli <116232175+patricioceli@users.noreply.github.com>
2023-09-18 12:41:25 -0500

    Update CompaniaTransportesEjemploC#.cs

--- CompaniaTransportesEjemploC#.cs
+++ CompaniaTransportesEjemploC#.cs
@@ -1,6 +1,81 @@
-"Desarrollo Avanzado: Explorando y Utilizando Patrones Creacionales"
+using System;
 
+// Producto (IEnvio)
+public interface IEnvio
+{
+    decimal CalcularCosto();
+    int CalcularTiempoEntrega();
+}
 
-"Innovación en Desarrollo de Software: Ejecución Práctica de Patrones Creacionales"
+// Producto Concreto (EnvioTerrestre)
+public class EnvioTerrestre : IEnvio
+{
+    public decimal CalcularCosto()
+    {
+        return 50.0M; // Tarifa base terrestre
+    }
 
-"Construyendo Software Innovador: Implementación de Patrones de Diseno Creacionales"
+    public int CalcularTiempoEntrega()
+    {
+        return 2; // Tiempo de entrega terrestre en días
+    }
+}
+
+// Producto Concreto (EnvioAereo)
+public class EnvioAereo : IEnvio
+{
+    public decimal CalcularCosto()
+    {
+        return 150.0M; // Tarifa base aérea
+    }
+
+    public int CalcularTiempoEntrega()
+    {
+        return 1; // Tiempo de entrega aérea en días
+    }
+}
+
+// Creador (CompaniaTransportes)
+public abstract class CompaniaTransportes
+{
+    public abstract IEnvio CrearEnvio();
+}
+
+// Creador Concreto (CompaniaTransportesTerrestres)
+public class CompaniaTransportesTerrestres : CompaniaTransportes
+{
+    public override IEnvio CrearEnvio()
+    {
+        return new EnvioTerrestre();
+    }
+}
+
+// Creador Concreto (CompaniaTransportesAereos)
+public class CompaniaTransportesAereos : CompaniaTransportes
+{
+    public override IEnvio CrearEnvio()
+    {
+        return new EnvioAereo();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        CompaniaTransportes compania = null;
+        IEnvio envio = null;
+
+        // Crear envío terrestre
+        compania = new CompaniaTransportesTerrestres();
+        envio = compania.CrearEnvio();
+        Console.WriteLine("Costo envío terrestre: " + envio.CalcularCosto());
+        Console.WriteLine("Tiempo de entrega envío terrestre: " + envio.CalcularTiempoEntrega() + " días");
+
+        // Crear envío aéreo
+        compania = new CompaniaTransportesAereos();
+        envio = compania.CrearEnvio();
+        Console.WriteLine("Costo envío aéreo: " + envio.CalcularCosto());
+        Console.WriteLine("Tiempo de entrega envío aéreo: " + envio.CalcularTiempoEntrega() + " días");
+    }
+}
